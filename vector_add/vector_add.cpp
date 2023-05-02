0071a3d8145272e0a594ee8a9d09962c56695ed8
Xenopelt <zhaomingtian@gmail.com>
2023-05-02 08:27:28 -0400

    add a new function

--- vector_add/vector_add.cpp
+++ vector_add/vector_add.cpp
@@ -3,6 +3,8 @@
 #include <numpy/arrayobject.h>
 
 extern "C" void vector_add_cuda(const float *a, const float *b, float *c, int n);
+extern "C" void vector_sub_cuda(const float *a, const float *b, float *c, int n);
+
 
 static PyObject *vector_add_py(PyObject *self, PyObject *args) {
     PyObject *py_a, *py_b, *py_c;
@@ -20,34 +22,49 @@ static PyObject *vector_add_py(PyObject *self, PyObject *args) {
     Py_RETURN_NONE;
 }
 
-static PyMethodDef vector_add_methods[] = {
-    {"vector_add", vector_add_py, METH_VARARGS, "Add two vectors using CUDA"},
-    {NULL, NULL, 0, NULL}
-};
 
-static struct PyModuleDef vector_add_module = {
-    PyModuleDef_HEAD_INIT,
-    "vector_add",
-    NULL,
-    -1,
-    vector_add_methods
-};
-
-PyMODINIT_FUNC PyInit_vector_add(void) {
-    PyObject *module = PyModule_Create(&vector_add_module);
-    if (!module) {
+static PyObject *vector_sub_py(PyObject *self, PyObject *args) { // Add this new function
+    PyObject *py_a, *py_b, *py_c;
+    if (!PyArg_ParseTuple(args, "OOO", &py_a, &py_b, &py_c)) {
         return NULL;
     }
 
-    import_array();
-    return module;
+    float *a = (float *)PyArray_DATA((PyArrayObject *)py_a);
+    float *b = (float *)PyArray_DATA((PyArrayObject *)py_b);
+    float *c = (float *)PyArray_DATA((PyArrayObject *)py_c);
+    int n = (int)PyArray_DIM((PyArrayObject *)py_a, 0);
+
+    vector_sub_cuda(a, b, c, n);
+
+    Py_RETURN_NONE;
 }
 
 
 static PyMethodDef methods[] = {
-    {"vector_add", (PyCFunction)vector_add_py, METH_VARARGS, "Add two vectors"},
+    {"vector_add", vector_add_py, METH_VARARGS, "Add two vectors using CUDA"},
+    {"vector_sub", vector_sub_py, METH_VARARGS, "Subtract two vectors using CUDA"}, 
     {NULL, NULL, 0, NULL}
 };
+
+// static struct PyModuleDef vector_add_module = {
+//     PyModuleDef_HEAD_INIT,
+//     "vector_add",
+//     NULL,
+//     -1,
+//     vector_add_methods
+// };
+
+// PyMODINIT_FUNC PyInit_vector_add(void) {
+//     PyObject *module = PyModule_Create(&vector_add_module);
+//     if (!module) {
+//         return NULL;
+//     }
+
+//     import_array();
+//     return module;
+// }
+
+
 
 PyMODINIT_FUNC PyInit_gpu(void) {
     PyObject *m;
--- vector_add/vector_add_cuda.cu
+++ vector_add/vector_add_cuda.cu
@@ -33,5 +33,35 @@ extern "C" {
         cudaFree(d_b);
         cudaFree(d_c);
     }
+
+    __global__ void vector_sub_kernel(const float *a, const float *b, float *c, int N) {
+        int i = blockIdx.x * blockDim.x + threadIdx.x;
+        if (i < N) {
+            c[i] = a[i] - b[i];
+        }
+    }
+
+    void vector_sub_cuda(const float *a, const float *b, float *c, int N) {
+        float *d_a, *d_b, *d_c;
+
+        cudaMalloc(&d_a, N * sizeof(float));
+        cudaMalloc(&d_b, N * sizeof(float));
+        cudaMalloc(&d_c, N * sizeof(float));
+
+        cudaMemcpy(d_a, a, N * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b, b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+        int blockSize = 256;
+        int numBlocks = (N + blockSize - 1) / blockSize;
+        vector_sub_kernel<<<numBlocks, blockSize>>>(d_a, d_b, d_c, N);
+
+        // Synchronize device
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(c, d_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaFree(d_a);
+        cudaFree(d_b);
+        cudaFree(d_c);
+    }
 }
 
